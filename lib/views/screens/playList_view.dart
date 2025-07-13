e5e55d621f25e86efca3faced127b9ce8282775a
Esmaeil <91015552+neji78@users.noreply.github.com>
2025-07-13 15:53:34 +0330

    set compatible system status bar color

--- lib/views/screens/playList_view.dart
+++ lib/views/screens/playList_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:music_player/views/playlist/recently_played.dart';
 import 'package:standard_searchbar/old/standard_searchbar.dart';
 
@@ -7,25 +8,27 @@ import '../../components/music_card.dart';
 class PlayListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        // appBar: AppBar(
-        //   title: Text("Play List"),
-        //   centerTitle: true,
-        // ),
-        backgroundColor: Color(0xff0F0817),
-        body: Container(
-          margin: EdgeInsets.only(top: 60, left: 20, right: 20),
-          child: Column(
-            spacing: 20,
-            children: [
-              StandardSearchBar(
-                height: 65,
-                backgroundColor: Colors.white.withAlpha(15),
-                shadow: [],
-              ),
-              RecentlyPlayed()
-            ],
-          ),
-        ));
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle(
+        statusBarColor: Color(0xff0F0817),
+        statusBarIconBrightness: Brightness.light,
+      ),
+      child: Scaffold(
+          backgroundColor: Color(0xff0F0817),
+          body: Container(
+            margin: EdgeInsets.only(top: 60, left: 20, right: 20),
+            child: Column(
+              spacing: 20,
+              children: [
+                StandardSearchBar(
+                  height: 65,
+                  backgroundColor: Colors.white.withAlpha(15),
+                  shadow: [],
+                ),
+                RecentlyPlayed()
+              ],
+            ),
+          )),
+    );
   }
 }
